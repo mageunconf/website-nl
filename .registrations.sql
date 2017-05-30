dd8291615317b1f7fafc77df9f409bdb98e7eb0e
Sander Mangel <sander@sandermangel.nl>
2017-05-30 17:29:25 +0200

    updated attendees

--- .registrations.sql
+++
@@ -1,4 +0,0 @@
-SELECT base_price_incl_tax, COUNT(*) FROM sales_flat_order_item i
-INNER JOIN sales_flat_order o ON o.entity_id = i.order_id
-WHERE status = 'complete' AND sku = 'mageunconf-2016'
-GROUP BY base_price_incl_tax
